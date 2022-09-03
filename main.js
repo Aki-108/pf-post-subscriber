4269ed6d85559ce9bd8464686a31501c57d58660
Aki <75360508+Aki-108@users.noreply.github.com>
2022-09-03 20:47:47 +0200

    Update main.js
    • fixed some small issues
    • changed unsub icon

--- main.js
+++ main.js
@@ -1,13 +1,13 @@
 // ==UserScript==
 // @name         Post Subscriber
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.7
 // @description  Get notified when there are new comments in a post.
 // @author       aki108
 // @match        https://www.pillowfort.social/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=pillowfort.social
-// @updateURL
-// @downloadURL
+// @updateURL    https://raw.githubusercontent.com/Aki-108/pf-post-subscriber/main/main.js
+// @downloadURL  https://raw.githubusercontent.com/Aki-108/pf-post-subscriber/main/main.js
 // @supportURL
 // @grant        none
 // ==/UserScript==
@@ -53,6 +53,7 @@
 
     var icon = document.createElement("div");
     icon.innerHTML = "<svg class='sidebar-img' viewBox='0 0 14 14' style='overflow:visible;'><path xmlns='http://www.w3.org/2000/svg' fill='none' stroke='#58b6dd' stroke-width='.8' d='M5.5 13.5h-3q-2 0 -2 -2v-8.5q0 -2 2 -2h8.5q2 0 2 2v5'></path><path xmlns='http://www.w3.org/2000/svg' fill='none' stroke='#58b6dd' stroke-width='.8' d='M3 4h7.5'></path><path xmlns='http://www.w3.org/2000/svg' fill='none' stroke='#58b6dd' stroke-width='.8' d='M3 7h4.5'></path><path xmlns='http://www.w3.org/2000/svg' fill='none' stroke='#58b6dd' stroke-width='.8' d='M3 10h4'></path><g xmlns='http://www.w3.org/2000/svg' transform='scale(0.5),translate(10,10)'><path xmlns='http://www.w3.org/2000/svg' d='M19.653 33.124h-2.817c-.289 0-.578-.02-.867-.04a.436.436 0 01-.307-.561c.177-.319.359-.635.544-.949.274-.466.559-.926.828-1.4.349-.609.7-1.217 1.022-1.839a2.149 2.149 0 00.185-.661 9.817 9.817 0 00.068-1.471c0-.871.011-1.743.02-2.614a6.175 6.175 0 01.5-2.445 6.93 6.93 0 01.986-1.622 6.661 6.661 0 013.694-2.288c.089-.022.127-.053.123-.151a2.576 2.576 0 01.081-.835 1.2 1.2 0 01.982-.915 1.319 1.319 0 011.068.219 1.282 1.282 0 01.514.863c.032.23.033.464.044.7 0 .059.012.087.082.1a7.247 7.247 0 011.569.574 6.471 6.471 0 011.342.888 7.087 7.087 0 011.473 1.787 5.493 5.493 0 01.564 1.28 7.837 7.837 0 01.226 1.125c.05.431.052.868.067 1.3.013.374.015.747.022 1.121l.021 1.216c.006.29.007.579.022.869a3.2 3.2 0 00.073.669 3.043 3.043 0 00.281.634c.2.375.42.742.636 1.11.288.491.583.977.871 1.468q.363.62.716 1.246a.4.4 0 01-.159.507.549.549 0 01-.358.084q-3.194.015-6.388.022c-.165 0-.159 0-.179.171a2.233 2.233 0 01-.607 1.324 2.071 2.071 0 01-1.319.672 2.211 2.211 0 01-1.678-.454 2.243 2.243 0 01-.822-1.365 1.308 1.308 0 01-.023-.217c0-.092-.03-.134-.134-.134-.99.013-1.978.012-2.966.012z' transform='translate(-15.024 -14.708)' style='fill:none;stroke:#58b6dd;stroke-width:1.6px'></path></g></svg>";
+    var iconUnsub = "<svg width='20px' height='23px' viewBox='0 0 20 20'><path xmlns='http://www.w3.org/2000/svg' d='M19.653 33.124h-2.817c-.289 0-.578-.02-.867-.04a.436.436 0 01-.307-.561c.177-.319.359-.635.544-.949.274-.466.559-.926.828-1.4.349-.609.7-1.217 1.022-1.839a2.149 2.149 0 00.185-.661 9.817 9.817 0 00.068-1.471c0-.871.011-1.743.02-2.614a6.175 6.175 0 01.5-2.445 6.93 6.93 0 01.986-1.622 6.661 6.661 0 013.694-2.288c.089-.022.127-.053.123-.151a2.576 2.576 0 01.081-.835 1.2 1.2 0 01.982-.915 1.319 1.319 0 011.068.219 1.282 1.282 0 01.514.863c.032.23.033.464.044.7 0 .059.012.087.082.1a7.247 7.247 0 011.569.574 6.471 6.471 0 011.342.888 7.087 7.087 0 011.473 1.787 5.493 5.493 0 01.564 1.28 7.837 7.837 0 01.226 1.125c.05.431.052.868.067 1.3.013.374.015.747.022 1.121l.021 1.216c.006.29.007.579.022.869a3.2 3.2 0 00.073.669 3.043 3.043 0 00.281.634c.2.375.42.742.636 1.11.288.491.583.977.871 1.468q.363.62.716 1.246a.4.4 0 01-.159.507.549.549 0 01-.358.084q-3.194.015-6.388.022c-.165 0-.159 0-.179.171a2.233 2.233 0 01-.607 1.324 2.071 2.071 0 01-1.319.672 2.211 2.211 0 01-1.678-.454 2.243 2.243 0 01-.822-1.365 1.308 1.308 0 01-.023-.217c0-.092-.03-.134-.134-.134-.99.013-1.978.012-2.966.012z' transform='translate(-15.024 -14.708)' style='fill:none;stroke:#58b6dd;stroke-width:1.7px'/><path stroke='#58b6dd' stroke-width='2px' d='M1 5.5l15 15'/><path stroke='white' stroke-width='1.5px' d='M0 2l20 20'/></svg>";
 
     function initGeneral() {
         if (document.getElementsByClassName("subscriptionicon").length > 0) return;
@@ -93,7 +94,7 @@
 
         if (document.URL.search("/posts/") == 29) {
             postID = document.URL.substring(36);
-            if (postID.indexOf("?") >= 0) postID = postID.substring(0, postID.indexOf("?")-1);
+            if (postID.indexOf("?") >= 0) postID = postID.substring(0, postID.indexOf("?"));
             subscribed = false;
             getData();
             subscriptionList.forEach(function(item) {
@@ -114,7 +115,7 @@
             $.getJSON("https://www.pillowfort.social/posts/"+postID+"/json", function(data) {
                 commentCount = data.comments_count;
                 postUser = data.username;
-                postTitle = data.title;
+                postTitle = data.title || "";
                 postDate = data.timestamp;
             });
         }
@@ -229,6 +230,7 @@
         $.getJSON("https://www.pillowfort.social/posts/"+id+"/json", function(json) {
             getData();
             let counter = 0;
+            document.getElementById("subscriptionnotification").style.display = "none";
             subscriptionList.forEach(function(data, index) {
                 if (data[0] == id) {
                     subscriptionList[index][2] = json.comments_count;
@@ -248,7 +250,7 @@
             document.getElementById("postsubscriberbackground").remove();
             document.getElementById("postsubscribermodal").remove();
         } else {
-            document.getElementById("subscriptionnotification").style.display = "none";
+            //document.getElementById("subscriptionnotification").style.display = "none";
             // Generate background
             let bg = document.createElement("div");
             bg.id = "postsubscriberbackground";
@@ -272,7 +274,7 @@
                 let entry = document.createElement("div");
                 entry.style.padding = "10px";
                 let unsubIcon = document.createElement("div");
-                unsubIcon.innerHTML = "<img class='svg-blue-dark' style='width:20px;' src='/assets/global/exit-af0d56e765e5d127ea19991ce19e59a1e356eaa323d56b8baf0554696f52c9bd.svg'>";
+                unsubIcon.innerHTML = iconUnsub;
                 unsubIcon.style.display = "inline-block";
                 unsubIcon.style.cursor = "pointer";
                 unsubIcon.title = "unsubscribe";
