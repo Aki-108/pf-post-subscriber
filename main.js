aff78f29a161032b18aa9b4055b896ab5bd4868a
Aki <75360508+Aki-108@users.noreply.github.com>
2022-09-04 11:52:13 +0200

    Update main.js
    • compacted code
    • added comments

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Post Subscriber
 // @namespace    http://tampermonkey.net/
-// @version      0.7
+// @version      0.8
 // @description  Get notified when there are new comments in a post.
 // @author       aki108
 // @match        https://www.pillowfort.social/*
@@ -43,11 +43,11 @@
     var postID = "";
     var subscribed = false;
     var subscriptionList = [];
+    var postData = [];
     var commentCount = 0;
     var postUser = "";
     var postTitle = "";
     var postDate = "";
-    var postData = [];
     var openTime = new Date().getTime();
     var updateIndex = 0;
 
@@ -55,12 +55,15 @@
     icon.innerHTML = "<svg class='sidebar-img' viewBox='0 0 14 14' style='overflow:visible;'><path xmlns='http://www.w3.org/2000/svg' fill='none' stroke='#58b6dd' stroke-width='.8' d='M5.5 13.5h-3q-2 0 -2 -2v-8.5q0 -2 2 -2h8.5q2 0 2 2v5'></path><path xmlns='http://www.w3.org/2000/svg' fill='none' stroke='#58b6dd' stroke-width='.8' d='M3 4h7.5'></path><path xmlns='http://www.w3.org/2000/svg' fill='none' stroke='#58b6dd' stroke-width='.8' d='M3 7h4.5'></path><path xmlns='http://www.w3.org/2000/svg' fill='none' stroke='#58b6dd' stroke-width='.8' d='M3 10h4'></path><g xmlns='http://www.w3.org/2000/svg' transform='scale(0.5),translate(10,10)'><path xmlns='http://www.w3.org/2000/svg' d='M19.653 33.124h-2.817c-.289 0-.578-.02-.867-.04a.436.436 0 01-.307-.561c.177-.319.359-.635.544-.949.274-.466.559-.926.828-1.4.349-.609.7-1.217 1.022-1.839a2.149 2.149 0 00.185-.661 9.817 9.817 0 00.068-1.471c0-.871.011-1.743.02-2.614a6.175 6.175 0 01.5-2.445 6.93 6.93 0 01.986-1.622 6.661 6.661 0 013.694-2.288c.089-.022.127-.053.123-.151a2.576 2.576 0 01.081-.835 1.2 1.2 0 01.982-.915 1.319 1.319 0 011.068.219 1.282 1.282 0 01.514.863c.032.23.033.464.044.7 0 .059.012.087.082.1a7.247 7.247 0 011.569.574 6.471 6.471 0 011.342.888 7.087 7.087 0 011.473 1.787 5.493 5.493 0 01.564 1.28 7.837 7.837 0 01.226 1.125c.05.431.052.868.067 1.3.013.374.015.747.022 1.121l.021 1.216c.006.29.007.579.022.869a3.2 3.2 0 00.073.669 3.043 3.043 0 00.281.634c.2.375.42.742.636 1.11.288.491.583.977.871 1.468q.363.62.716 1.246a.4.4 0 01-.159.507.549.549 0 01-.358.084q-3.194.015-6.388.022c-.165 0-.159 0-.179.171a2.233 2.233 0 01-.607 1.324 2.071 2.071 0 01-1.319.672 2.211 2.211 0 01-1.678-.454 2.243 2.243 0 01-.822-1.365 1.308 1.308 0 01-.023-.217c0-.092-.03-.134-.134-.134-.99.013-1.978.012-2.966.012z' transform='translate(-15.024 -14.708)' style='fill:none;stroke:#58b6dd;stroke-width:1.6px'></path></g></svg>";
     var iconUnsub = "<svg width='20px' height='23px' viewBox='0 0 20 20'><path xmlns='http://www.w3.org/2000/svg' d='M19.653 33.124h-2.817c-.289 0-.578-.02-.867-.04a.436.436 0 01-.307-.561c.177-.319.359-.635.544-.949.274-.466.559-.926.828-1.4.349-.609.7-1.217 1.022-1.839a2.149 2.149 0 00.185-.661 9.817 9.817 0 00.068-1.471c0-.871.011-1.743.02-2.614a6.175 6.175 0 01.5-2.445 6.93 6.93 0 01.986-1.622 6.661 6.661 0 013.694-2.288c.089-.022.127-.053.123-.151a2.576 2.576 0 01.081-.835 1.2 1.2 0 01.982-.915 1.319 1.319 0 011.068.219 1.282 1.282 0 01.514.863c.032.23.033.464.044.7 0 .059.012.087.082.1a7.247 7.247 0 011.569.574 6.471 6.471 0 011.342.888 7.087 7.087 0 011.473 1.787 5.493 5.493 0 01.564 1.28 7.837 7.837 0 01.226 1.125c.05.431.052.868.067 1.3.013.374.015.747.022 1.121l.021 1.216c.006.29.007.579.022.869a3.2 3.2 0 00.073.669 3.043 3.043 0 00.281.634c.2.375.42.742.636 1.11.288.491.583.977.871 1.468q.363.62.716 1.246a.4.4 0 01-.159.507.549.549 0 01-.358.084q-3.194.015-6.388.022c-.165 0-.159 0-.179.171a2.233 2.233 0 01-.607 1.324 2.071 2.071 0 01-1.319.672 2.211 2.211 0 01-1.678-.454 2.243 2.243 0 01-.822-1.365 1.308 1.308 0 01-.023-.217c0-.092-.03-.134-.134-.134-.99.013-1.978.012-2.966.012z' transform='translate(-15.024 -14.708)' style='fill:none;stroke:#58b6dd;stroke-width:1.7px'/><path stroke='#58b6dd' stroke-width='2px' d='M1 5.5l15 15'/><path stroke='white' stroke-width='1.5px' d='M0 2l20 20'/></svg>";
 
+    //run once when the post is loaded
     function initGeneral() {
+        //only run once
         if (document.getElementsByClassName("subscriptionicon").length > 0) return;
+
+        //add button to collapsed sidebar
         let sidebarSmall = document.getElementsByClassName("sidebar-collapsed")[1];
         let subscriptionSmall = document.createElement("a");
-        subscriptionSmall.classList.add("subscriptionicon");
-        subscriptionSmall.classList.add("sidebar-icon");
+        subscriptionSmall.classList.add("subscriptionicon", "sidebar-icon");
         subscriptionSmall.title = "Subscriptions";
         subscriptionSmall.style.cursor = "pointer";
         subscriptionSmall.appendChild(icon.cloneNode(true));
@@ -72,11 +75,11 @@
         subscriptionSmall.addEventListener("click", showPopup);
         sidebarSmall.appendChild(subscriptionSmall);
 
+        //add button to expanded sidebar
         let sidebarBig = document.getElementsByClassName("sidebar-expanded")[2];
         sidebarBig.children[9].style.paddingBottom = "0";
         let subscriptionBig = document.createElement("div");
-        subscriptionBig.classList.add("subscriptionicon");
-        subscriptionBig.classList.add("sidebar-topic");
+        subscriptionBig.classList.add("subscriptionicon", "sidebar-topic");
         subscriptionBig.appendChild(icon.cloneNode(true).children[0]);
         let title = document.createElement("a");
         title.addEventListener("click", showPopup);
@@ -91,16 +94,18 @@
         subscriptionBig.appendChild(counter);
         sidebarBig.appendChild(subscriptionBig);
 
-
+        getData();
         if (document.URL.search("/posts/") == 29) {
+            //check if this post is subscribed
             postID = document.URL.substring(36);
+            if (postID.indexOf("/") >= 0) postID = postID.substring(0, postID.indexOf("/"));
             if (postID.indexOf("?") >= 0) postID = postID.substring(0, postID.indexOf("?"));
             subscribed = false;
-            getData();
             subscriptionList.forEach(function(item) {
                 if (item[0] == postID) subscribed = true;
             });
 
+            //add button to post navigation
             let postNav = document.getElementsByClassName("post-nav")[0];
             let subscriptionNav = document.createElement("span");
             subscriptionNav.id = "subscribebutton";
@@ -112,15 +117,16 @@
             postNav.appendChild(subscriptionNav);
             if (subscribed) document.getElementById("subscribebutton").firstChild.classList.add("svg-pink-light");
 
+            //get posts information in preparation for a new subscription
             $.getJSON("https://www.pillowfort.social/posts/"+postID+"/json", function(data) {
                 commentCount = data.comments_count;
-                postUser = data.username;
+                postUser = data.username || "ERROR";
                 postTitle = data.title || "";
-                postDate = data.timestamp;
+                postDate = data.timestamp || "ERROR";
             });
         }
 
-        getData();
+        //update one post from the subscription list every two minutes, starting at a random position
         updateIndex = Math.floor(Math.random()*subscriptionList.length);
         checkPost(subscriptionList[updateIndex][0]);
         window.setInterval(function() {
@@ -132,9 +138,12 @@
         }, 120000);
     }
 
+    //run everytime the comment section is loaded
     function initComments() {
         if (subscribed) {
             highlightComments();
+
+            //remeber when the comments were viewed to detect new ones accordingly
             //source: https://stackoverflow.com/a/14746878
             window.addEventListener("beforeunload", function(evt) {
                 evt.returnValue = '';
@@ -149,15 +158,19 @@
         }
     }
 
+    //(un-)subscribe to a post
     function toggleSubscription() {
         if (subscribed) {
             unsubscribe(postID);
+            //change state of the button in the post navigation
             document.getElementById("subscribebutton").firstChild.classList.remove("svg-pink-light");
             document.getElementById("subscribebutton").title = "subscribe";
         } else {
+            //save post data locally
             getData();
             let commentData = [postID, commentCount, commentCount, openTime];
             subscriptionList.push(commentData);
+            //remove , and ; from the data so it can't mess with the array formating
             postUser = postUser.replaceAll(",",".").replaceAll(";",":");
             postTitle = postTitle.replaceAll(",",".").replaceAll(";",":");
             postDate = postDate.replaceAll(",",".").replaceAll(";",":");
@@ -169,6 +182,7 @@
             });
             list = list.toString();
             localStorage.setItem("postsubscriptiondata", list);
+            //change state of the button in the post navigation
             document.getElementById("subscribebutton").firstChild.classList.add("svg-pink-light");
             document.getElementById("subscribebutton").title = "unsubscribe";
             setData();
@@ -176,6 +190,7 @@
         subscribed = !subscribed;
     }
 
+    //load data from localstorage and make it an array
     function getData() {
         subscriptionList = [];
         if (localStorage.getItem("postsubscriptions")) {
@@ -193,6 +208,7 @@
         }
     }
 
+    //make data a string and store it locally
     function setData() {
         let list = [];
         subscriptionList.forEach(function(value) {
@@ -202,6 +218,7 @@
         localStorage.setItem("postsubscriptions", list);
     }
 
+    //add the "new" marking to comments
     function highlightComments() {
         let pivotTime = 0;
         subscriptionList.forEach(function(data) {
@@ -216,16 +233,13 @@
             if (time > pivotTime) {
                 let newIcon = document.createElement("div");
                 newIcon.innerHTML = "new";
-                newIcon.style.height = 0;
-                newIcon.style.margin = "-25px -53px";
-                newIcon.style.color = "#ff7fc5";
-                newIcon.style.fontWeight = "bold";
-                newIcon.style.fontSize = "1.5em";
+                newIcon.setAttribute("style", "height:0;margin:-25px -53px;color:#ff7fc5;font-weight:bold;font-size:1.5em;");
                 comments[a].children[0].appendChild(newIcon);
             }
         }
     }
 
+    //get data from a post
     function checkPost(id) {
         $.getJSON("https://www.pillowfort.social/posts/"+id+"/json", function(json) {
             getData();
@@ -245,30 +259,30 @@
         });
     }
 
+    //toggle the popup
     function showPopup() {
         if (document.getElementById("postsubscriberbackground")) {
+            //remove the popup if it's already there
             document.getElementById("postsubscriberbackground").remove();
             document.getElementById("postsubscribermodal").remove();
         } else {
-            //document.getElementById("subscriptionnotification").style.display = "none";
-            // Generate background
+            //generate background
             let bg = document.createElement("div");
             bg.id = "postsubscriberbackground";
-            bg.classList.add("modal-backdrop");
-            bg.classList.add("in");
+            bg.classList.add("modal-backdrop", "in");
             document.getElementsByTagName("body")[0].appendChild(bg);
 
+            //generate modal basis
             let modal = document.createElement("div");
             modal.id = "postsubscribermodal";
-            modal.classList.add("modal");
-            modal.classList.add("in");
-            modal.style.display = "block";
-            modal.style.zIndex = "3";
-            modal.style.overflow = "auto";
-            modal.innerHTML = "<div class='modal-dialog'><div class='modal-content' id='postsubscribermodalcontent'></div></div>";
+            modal.classList.add("modal", "in");
+            modal.setAttribute("style", "display:block;z-index:3;overflow:auto;");
+            //set the header
+            modal.innerHTML = "<div class='modal-dialog'><div class='modal-content' id='postsubscribermodalcontent'><div style='padding:10px 15px 0 40px;border-bottom:2px solid #e3e3e3;'><button class='close' type='button' title='Close'><span>x</span></button><div style='float:right;margin:10px 20px 0 0;color:#575757;'>unsubscribe all</div><h4 class='modal-title'>Subscriptions</h4></div></div></div>";
             document.getElementsByTagName("body")[0].appendChild(modal);
             document.getElementById("postsubscribermodal").addEventListener("click", showPopup);
 
+            //generate post entries
             getData();
             subscriptionList.forEach(function(data, index) {
                 let entry = document.createElement("div");
@@ -298,6 +312,7 @@
         }
     }
 
+    //remove a subscription
     function unsubscribe(id) {
         getData();
         subscriptionList.forEach(function(data, index) {
